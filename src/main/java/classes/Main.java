0273af11e19b8c6918b673b5dc12cbf454f5fa0d
CJQuintero61 <cjquintero61@gmail.com>
2025-04-02 12:58:24 -0500

    setup structure

---
+++ src/main/java/classes/Main.java
@@ -0,0 +1,12 @@
+// Developer: CJ Quintero
+// Last Updated
+package classes;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    } // end main
+
+} // end class
